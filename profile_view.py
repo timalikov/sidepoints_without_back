c5e115f3bdfe326dda9a2ce49356dfcbde4c3a34
Sabyr Kabylbek <skab0015@student.monash.edu>
2024-06-16 18:18:45 +1000

    without push

--- profile_view.py
+++ profile_view.py
@@ -3,4 +3,4 @@ import discord
 class ProfileView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)  # Set timeout=None if you don't want the buttons to expire
-        self.add_item(discord.ui.Button(label="Go to Profile", url="https://sidekick.fans/dashboard", style=discord.ButtonStyle.url))
+        self.add_item(discord.ui.Button(label="Go to Profile", url="https://app.sidekick.fans/", style=discord.ButtonStyle.url))
