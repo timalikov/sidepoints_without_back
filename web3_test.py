8617791b8091c5682b5c9abfe84b7b7789ac3923
Sabyr Kabylbek <skab0015@student.monash.edu>
2024-07-25 22:37:17 +1000

    test

--- web3_test.py
+++ web3_test.py
@@ -59,78 +59,78 @@ import boto3
 
 import config
 
-client = boto3.client(
-    'sqs',
-    aws_access_key_id="AKIARQOJDAVZLXZGJB4Q",
-    aws_secret_access_key="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",
-    region_name='eu-central-1'
-)
-
-QUEUE_URL = "https://sqs.eu-central-1.amazonaws.com/104037811570/sidekick_bot_deliveries.fifo"
-
-
-def receive_messages():
-    messages_list = []
-
-    try:
-        response = client.receive_message(
-            QueueUrl=QUEUE_URL,
-            MaxNumberOfMessages=10,
-            WaitTimeSeconds=10
-        )
-
-        if 'Messages' in response:
-            for message in response['Messages']:
-                body = json.loads(message['Body'])
-                messages_list.append(body)
-
-                client.delete_message(
-                    QueueUrl=QUEUE_URL,
-                    ReceiptHandle=message['ReceiptHandle']
-                )
-        # else:
-        #     print("No messages available.")
-    except Exception as e:
-        print("Error receiving messages:", e)
-
-    return messages_list
-
-def send_message_to_endpoint(message_body, port_id):
-    url = f"http://localhost:{port_id}/create_private_channel"
-    max_retries = 10
-    retry_count = 0
-    wait_time = 1
-
-    while retry_count < max_retries:
-        try:
-            response = requests.post(url, json=message_body)
-            if response.status_code == 200:
-                print(f"Message sent successfully: {message_body}")
-                return True
-            else:
-                print(f"Failed to send message, status code: {response.status_code}")
-        except Exception as e:
-            print(f"Exception occurred: {e}")
-
-        retry_count += 1
-        time.sleep(wait_time)
-        wait_time *= 2
-
-    print(f"Failed to create channel for message: {message_body['channelName']}")
-    return False
-
-def process_messages():
-    port_id = config.PORT_ID
-
-    while True:
-        messages = receive_messages()
-
-        for message in messages:
-            send_message_to_endpoint(message, port_id)
-        time.sleep(5)
-
-def main():
-    process_messages()
-
-if __name__ == '__main__':
-    main()
+# client = boto3.client(
+#     'sqs',
+#     aws_access_key_id="AKIARQOJDAVZLXZGJB4Q",
+#     aws_secret_access_key="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",
+#     region_name='eu-central-1'
+# )
+#
+# QUEUE_URL = "https://sqs.eu-central-1.amazonaws.com/104037811570/sidekick_bot_deliveries.fifo"
+#
+#
+# def receive_messages():
+#     messages_list = []
+#
+#     try:
+#         response = client.receive_message(
+#             QueueUrl=QUEUE_URL,
+#             MaxNumberOfMessages=10,
+#             WaitTimeSeconds=10
+#         )
+#
+#         if 'Messages' in response:
+#             for message in response['Messages']:
+#                 body = json.loads(message['Body'])
+#                 messages_list.append(body)
+#
+#                 client.delete_message(
+#                     QueueUrl=QUEUE_URL,
+#                     ReceiptHandle=message['ReceiptHandle']
+#                 )
+#         # else:
+#         #     print("No messages available.")
+#     except Exception as e:
+#         print("Error receiving messages:", e)
+#
+#     return messages_list
+#
+# def send_message_to_endpoint(message_body, port_id):
+#     url = f"http://localhost:{port_id}/create_private_channel"
+#     max_retries = 10
+#     retry_count = 0
+#     wait_time = 1
+#
+#     while retry_count < max_retries:
+#         try:
+#             response = requests.post(url, json=message_body)
+#             if response.status_code == 200:
+#                 print(f"Message sent successfully: {message_body}")
+#                 return True
+#             else:
+#                 print(f"Failed to send message, status code: {response.status_code}")
+#         except Exception as e:
+#             print(f"Exception occurred: {e}")
+#
+#         retry_count += 1
+#         time.sleep(wait_time)
+#         wait_time *= 2
+#
+#     print(f"Failed to create channel for message: {message_body['channelName']}")
+#     return False
+#
+# def process_messages():
+#     port_id = config.PORT_ID
+#
+#     while True:
+#         messages = receive_messages()
+#
+#         for message in messages:
+#             send_message_to_endpoint(message, port_id)
+#         time.sleep(5)
+#
+# def main():
+#     process_messages()
+#
+# if __name__ == '__main__':
+#     main()
