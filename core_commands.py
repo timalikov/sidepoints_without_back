0c4f0904959f079d19e1b364bbfb189c724f2a0a
Temirlan Malikov <timalikov@Temirlans-MacBook-Air.local>
2024-10-04 10:17:37 +0500

    added category

--- core_commands.py
+++ core_commands.py
@@ -173,12 +173,13 @@ async def get_guild_invite_link(guild_id):
 
 @bot.tree.command(name="order", description="Use this command to post your service request and summon Kickers to take the order.")
 @app_commands.choices(choices=[app_commands.Choice(name="All players", value="ALL"),
-                               app_commands.Choice(name="Buddy", value="57c86488-8935-4a13-bae0-5ca8783e205d"),
+                               app_commands.Choice(name="Casual", value="57c86488-8935-4a13-bae0-5ca8783e205d"),
                                app_commands.Choice(name="Coaching", value="88169d78-85b4-4fa3-8298-3df020f13a6f"),
                                app_commands.Choice(name="Just Chatting", value="2974b0e8-69de-4d7c-aa4d-d5aa8e05d360"),
                                app_commands.Choice(name="Watch Youtube", value="d3ae39d2-fd86-41d7-bc38-0b582ce338b5"),
                                app_commands.Choice(name="Play Games", value="79bf303a-318b-4815-bd56-7b0b49ae7bff"),
-                               app_commands.Choice(name="Virtual Date", value="d6b9fc04-bfb2-46df-88eb-6e8c149e34d9")
+                               app_commands.Choice(name="Virtual Date", value="d6b9fc04-bfb2-46df-88eb-6e8c149e34d9"),
+                               app_commands.Choice(name="World Of Tanks", value="2e851835-c033-4c90-a920-ffa75318235a")
                                ])
 async def order(interaction: discord.Interaction, choices: app_commands.Choice[str]):
     guild_id = interaction.guild.id
