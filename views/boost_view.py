c9454d271a16d7e4eaafacde9c41fa76355d2b63
Temirlan Malikov <timalikov@Temirlans-MacBook-Air.local>
2024-10-16 18:01:40 +0500

    boost fix

--- views/boost_view.py
+++ views/boost_view.py
@@ -10,7 +10,7 @@ from config import APP_CHOICES, FORUM_NAME
 from translate import translations
 
 from database.dto.sql_forum_posted import ForumUserPostDatabase
-from message_constructors import create_profile_embed_2
+from message_constructors import create_profile_embed
 from models.forum import find_forum
 from database.dto.psql_services import Services_Database
 
@@ -36,7 +36,7 @@ class BoostView(View):
     async def initialize(self):
         self.user_data = await self.service_db.get_next_service()
         if self.user_data:
-            self.profile_embed = create_profile_embed_2(self.user_data, lang=self.lang)
+            self.profile_embed = create_profile_embed(self.user_data, lang=self.lang)
         else:
             self.no_user = True
 
@@ -52,7 +52,7 @@ class BoostView(View):
 
         payment_link = f"{os.getenv('WEB_APP_URL')}/boost/{self.user_data['profile_id']}?side_auth=DISCORD"
         await interaction.followup.send(
-            translations["boost_profile_link"][interaction.user.lang].format(payment_link=payment_link), 
+            translations["boost_profile_link"][self.lang].format(boost_link=payment_link), 
             ephemeral=True
         )
 
@@ -67,7 +67,7 @@ class BoostView(View):
 
         self.user_data = await self.service_db.get_next_service()
 
-        self.profile_embed = create_profile_embed_2(self.user_data, lang=self.lang)
+        self.profile_embed = create_profile_embed(self.user_data, lang=self.lang)
         await interaction.edit_original_response(embed=self.profile_embed, view=self)
 
     @discord.ui.button(label="Share", style=discord.ButtonStyle.secondary, custom_id="share_kicker")
@@ -81,29 +81,21 @@ class BoostView(View):
 
         user_id = self.user_data['discord_id']
         forum = await find_forum(guild=interaction.guild, forum_name=FORUM_NAME)
-        thread_id = await ForumUserPostDatabase.get_thread_id_by_user_and_server(user_id, str(main_guild_id))
+        thread_id = await ForumUserPostDatabase.get_thread_id_by_user_and_server(user_id, interaction.guild.id)
 
+        message: str = ""
         if thread_id:
             thread = forum.get_thread(int(thread_id))
-            profile_link = thread.jump_url
-            if interaction.response.is_done():
-                await interaction.followup.send(
-                    translations["profile_account"][interaction.user.lang].format(profile_link=profile_link), 
-                    ephemeral=True
-                )
+            if not thread:
+                message = translations["profile_not_found"][self.lang]
             else:
-                await interaction.response.send_message(
-                    translations["profile_account"][interaction.user.lang].format(profile_link=profile_link), 
-                    ephemeral=True
+                message = (
+                    translations["share_profile_account"][self.lang]
+                    .format(profile_link=thread.jump_url)
                 )
         else:
-            if interaction.response.is_done():
-                await interaction.followup.send(
-                    translations["sidekicker_account_not_posted"][interaction.user.lang], 
-                    ephemeral=True
-                )
-            else:
-                await interaction.response.send_message(
-                    translations["sidekicker_account_not_posted"][interaction.user.lang], 
-                    ephemeral=True
-                )
+            message = translations["sidekicker_account_not_posted"][self.lang]
+        if interaction.response.is_done():
+            await interaction.followup.send(message, ephemeral=True)
+        else:
+            await interaction.response.send_message(message, ephemeral=True)
