05626f479ba8422d31e0f1c2664d42a3b4d83078
Evgeniy <e.tabashnyuk>
2024-10-29 11:04:51 +0500

    merge main

---
+++ database/dto/psql_guild_channels.py
@@ -0,0 +1,45 @@
+# from database.dto.base import BasePsqlDTO
+
+
+# class DiscordGuildChannelsDTO(BasePsqlDTO):
+#     """
+#     DiscordGuildChannelsDTO!
+
+#     Info: 
+#         schema: discord_bot;
+#         locate: tables;
+#         name: guild_channels.
+#     Fields:
+#         id: int
+#         guild_id: int
+#         channel_id: int
+#         channel_name: varchar
+#     """
+
+#     BASE_QUERY = "SELECT * FROM discord_bot.guild_channels"
+
+#     async def get_channel_id_by_name(self, guild_id: int, channel_name: str) -> int:
+#         async with self.get_connection() as conn:
+#             query = self.BASE_QUERY.replace("*", "channel_id") + " WHERE guild_id = $1 AND channel_name = $2"
+#             channels = await conn.fetch(query, guild_id, channel_name)
+#         try:
+#             channel = channels[0]["channel_id"]
+#         except (IndexError, KeyError):
+#             channel = None
+#         return channel
+    
+#     async def create(self, guild_id: int, channel_name: str, channel_id: int) -> str:
+#         async with self.get_connection() as conn:
+#             query = (
+#                 "INSERT INTO discord_bot.guild_channels "
+#                 "(guild_id, channel_name, channel_id) "
+#                 "VALUES ($1, $2, $3)"
+#             )
+#             new_row = await conn.fetch(query, guild_id, channel_name, channel_id)
+#         return new_row
+    
+#     async def delete(self, channel_id: int) -> str:
+#         async with self.get_connection() as conn:
+#             query = "DELETE FROM discord_bot.guild_channels WHERE channel_id = $1"
+#             new_row = await conn.fetch(query, channel_id)
+#         return new_row
--- models/public_channel.py
+++ models/public_channel.py
@@ -4,6 +4,18 @@ from models.category import (
     get_or_create_category_by_name,
     get_category_by_name
 )
+# from database.dto.psql_guild_channels import DiscordGuildChannelsDTO
+
+
+# async def _get_or_delete_channel_by_id(
+#     guild: discord.Guild,
+#     channel_id: int
+# ) -> discord.ChannelType:
+#     dto = DiscordGuildChannelsDTO()
+#     channel_from_database = guild.get_channel(channel_id)
+#     if not channel_from_database:
+#         await dto.delete(channel_id)
+#     return channel_from_database
 
 
 async def find_channel_by_category_and_name(
@@ -13,12 +25,53 @@ async def find_channel_by_category_and_name(
 ) -> discord.ChannelType:
     """Finds a channel by its category and name."""
 
-    current_category: discord.CategoryChannel = await get_category_by_name(
-        guild=guild, category_name=category_name
-    )
-    if not current_category:
+    async def _get_channel_from_db(
+        channel_name: str,
+        guild: discord.Guild,
+    ) -> discord.ChannelType:
         return None
-    return discord.utils.get(current_category.channels, name=channel_name)
+        # dto = DiscordGuildChannelsDTO()
+        # channel_id_from_db: int = await dto.get_channel_id_by_name(
+        #     guild_id=guild.id, channel_name=channel_name
+        # )
+        # channel: discord.ChannelType = None
+        # if channel_id_from_db:
+        #     channel = await _get_or_delete_channel_by_id(
+        #         guild=guild, channel_id=channel_id_from_db
+        #     )
+        # return channel
+            
+    async def _get_channel_from_discord(
+        category_name: str,
+        channel_name: str,
+        guild: discord.Guild,
+    ) -> discord.ChannelType:
+        current_category: discord.CategoryChannel = await get_category_by_name(
+            guild=guild, category_name=category_name
+        )
+        if not current_category:
+            return None
+        channel: discord.TextChannel = discord.utils.get(
+            current_category.channels,
+            name=channel_name
+        )
+        return channel
+
+    channel_from_db = await _get_channel_from_db(
+        guild=guild,
+        channel_name=channel_name
+    )
+    if channel_from_db:
+        return channel_from_db
+    channel_from_discord = await _get_channel_from_discord(
+        guild=guild,
+        channel_name=channel_name,
+        category_name=category_name
+    )
+    # if channel_from_discord and not channel_from_db:
+    #     dto = DiscordGuildChannelsDTO()
+    #     await dto.create(guild.id, channel_name, channel_from_discord.id)
+    return channel_from_discord
 
 
 async def get_or_create_channel_by_category_and_name(
@@ -36,4 +89,6 @@ async def get_or_create_channel_by_category_and_name(
             guild=guild, category_name=category_name
         )
         channel = await guild.create_text_channel(name=channel_name, category=category)
+        # dto = DiscordGuildChannelsDTO()
+        # await dto.create(guild.id, channel_name, channel.id)
     return channel
