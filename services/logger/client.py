72c973d9e2aa96ec2a4d526fb1b447f19baf03ab
Evgeniy <e.tabashnyuk>
2024-11-23 20:13:26 +0500

    add discord logs and free top up

--- services/logger/client.py
+++ services/logger/client.py
@@ -1,13 +1,39 @@
+import discord
+
 from logging import getLogger
+
+from bot_instance import get_bot
+from config import LOGGER_DISCORD_USERS_LOGS, LOGGER_SEND_DISCORD_DM_LOGS
 
 from requests import Response
 
 logger = getLogger("")
+bot = get_bot()
+
 
 class CustomLogger:
     """
     Custom class for logging data!
     """
+
+    @staticmethod
+    async def http_discord(
+        place: str,
+        response: Response
+    ) -> None:
+        test_embed = discord.Embed(
+            title="Backend response",
+            colour=discord.Colour.red()
+        )
+        test_embed.add_field(name="WHERE", value=place)
+        test_embed.add_field(name="Status code", value=response.status_code)
+        test_embed.add_field(name="Message", value=response.text)
+        test_embed.add_field(name="Request body", value=response.request.body)
+        for user_id in LOGGER_DISCORD_USERS_LOGS:
+            user = bot.get_user(user_id)
+            if not user:
+                continue
+            await user.send(embed=test_embed)
 
     @staticmethod
     def _get_http_message(
@@ -23,7 +49,35 @@ class CustomLogger:
         )
     
     @staticmethod
-    def http_error(
+    async def http_error(
+        place: str,
+        response: Response
+    ) -> None:
+        if LOGGER_SEND_DISCORD_DM_LOGS:
+            await CustomLogger.http_discord(place, response)
+        logger.error(
+            CustomLogger._get_http_message(
+                place=place,
+                response=response,
+            )
+        )
+    
+    @staticmethod
+    async def http_warning(
+        place: str,
+        response: Response
+    ) -> None:
+        if LOGGER_SEND_DISCORD_DM_LOGS:
+            await CustomLogger.http_discord(place, response)
+        logger.warning(
+            CustomLogger._get_http_message(
+                place=place,
+                response=response,
+            )
+        )
+
+    @staticmethod
+    async def http_error_sync(
         place: str,
         response: Response
     ) -> None:
@@ -35,7 +89,7 @@ class CustomLogger:
         )
     
     @staticmethod
-    def http_warning(
+    async def http_warning_sync(
         place: str,
         response: Response
     ) -> None:
