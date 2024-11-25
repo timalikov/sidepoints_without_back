fdb7d72bf00e32f33d7952ae2ab77eda08ce2f14
Temirlan Malikov <timalikov@Temirlans-MacBook-Air.local>
2024-11-25 10:01:17 +0500

    fix(points_command): fix + refactoring

--- commands/points_command.py
+++ commands/points_command.py
@@ -40,14 +40,15 @@ class PointsCommand(commands.Cog):
             dto = LeaderboardDatabase()
             user_ranking = await dto.get_user_ranking(profile_id=profile_id)
             
-            total_points = user_ranking.get('total_score', 0) if user_ranking else 0
-            rank = user_ranking.get('total_pos', 0) if user_ranking else 0
-            view = PointsView(
-                username=username,
-                user=interaction.user,
-                total_points=total_points, 
-                rank=rank, 
-                lang=lang
-            )
+        total_points = user_ranking.get('total_score', 0)
+        rank = user_ranking.get('total_pos', 0)
+        
+        view = PointsView(
+            username=username,
+            user=interaction.user,
+            total_points=total_points, 
+            rank=rank, 
+            lang=lang
+        )
         send_method = interaction.followup.send if interaction.response.is_done() else interaction.response.send_message
         view.message = await send_method(embed=view.embed_message, view=view)
