20fcaf205ac7ea0b68f45159a6329fb048e82b57
Temirlan Malikov <timalikov@Temirlans-MacBook-Air.local>
2024-11-25 13:08:50 +0500

    fix

--- services/cogs/invite_tracker.py
+++ services/cogs/invite_tracker.py
@@ -8,9 +8,10 @@ import json
 from config import INVITE_LOGS_CHANNEL_ID, MAIN_GUILD_ID
 from services.sqs_client import SQSClient
 from translate import get_lang_prefix, translations
+from logging import getLogger
 
 bot = get_bot()
-
+logger = getLogger("")
 class InviteTracker(commands.Cog):
     """
     Tracks invites and logs which invite was used when a member joins or leaves the server.
@@ -93,8 +94,15 @@ class InviteTracker(commands.Cog):
                     await inviter.send(translations["link_invite_message"][lang].format(member_name=member.name,used_invite_code=used_invite.code))
                     await self.services_db.save_user_reward(discord_id=inviter.id, reward_type="DISCORD_INVITE", server_id=0, invited_discord_id=member.id)
                 
-                await logs.send(embed=embed)
-            
+                if logs is None:
+                    logger.error("Logs channel not found. Check `self.logs_channel` value.")
+                    return
+                try:
+                    await logs.send(embed=embed)
+                except discord.Forbidden:
+                    self.bot.logger.error("Bot lacks permission to send messages in the logs channel.")
+                except discord.HTTPException as e:
+                    self.bot.logger.error(f"Failed to send message to logs channel: {e}")
                 
             else:
                 embed.add_field(
